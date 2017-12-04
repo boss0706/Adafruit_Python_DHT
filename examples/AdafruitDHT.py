2052c439475772e3e23a5d54a7e061fd2a553cd9
Boss0706 <h0936678293@gmail.com>
2017-12-04 13:04:21 +0800

    04050706(2)

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -20,9 +20,30 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import sys
-
+import httplib, urllib
 import Adafruit_DHT
 import time
+import json
+
+deviceId =  "DBF79Bgr"
+deviceKey = "MfhYHJj2NogiXDVE"
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print "Error: %s" % ex
+			time.sleep(10) 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
+
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -40,6 +61,7 @@ else:
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -50,6 +72,9 @@ while True:
 # If this happens try again!
 	if humidity is not None and temperature is not None:
     		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
+		        {"dataChnId":"Temperature","values":{"value":t0}}]} 
+		post_to_mcs(payload)
 		time.sleep(1)
 	else:
     		print('Failed to get reading. Try again!')
